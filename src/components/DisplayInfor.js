226199abfe86d85644ad4ddc714f633ad1675583
hieu <kaoha45@gmail.com>
2025-08-25 16:54:04 +0700

    video 23

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,22 +2,45 @@ import React from "react";
 import UserInfor from "./UserInfor";
 
 class DisplayInfor extends React.Component {
+
+    state = {
+        isShowListUser: true
+    }
+    handleShowHide=()=>{
+        this.setState({
+            isShowListUser: !this.state.isShowListUser // them dau cham than thi thanh toggle tat/bat
+        })
+    }
+
     render() {
         // destructuring props
         const { listUser } = this.props;
-
-        return (
-            <>
+        //console.log(listUser)
+        //console.table(listUser)
+        return (            
+            <div>
+                <div>
+                    <spam onClick={()=>{this.handleShowHide()}}>
+                        {this.state.isShowListUser === true? "hide list users" : " show list users"}
+                        </spam>
+                </div>
+            {this.state.isShowListUser &&  
+                <div>
                 {listUser.map((user, index) => {
-                    return (
-                        <div key={user.id}>
-                            <div>tôi là {user.name}</div>
-                            <div>tôi {user.age} tuổi</div>
-                            <hr />
-                        </div>
-                    );
+                    console.log("check map user", user)
+
+                        return (
+                            <div key={user.id} className={+user.age >18 ? "green" : "red"}>
+                                <div>tôi là {user.name}</div>
+                                <div>tôi {user.age} tuổi</div>
+                                <hr />
+                            </div>
+                        );
+                    
                 })}
-            </>
+                </div>
+             }
+            </div>
         );
     }
 }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,8 +6,8 @@ class MyComponent extends React.Component {
     state = {
         listUser: [
             { id: 1, name: "abc", age: "30" },
-            { id: 2, name: "d", age: "24" },
-            { id: 3, name: "e", age: "25" }
+            { id: 2, name: "d", age: "10" },
+            { id: 3, name: "e", age: "60" }
         ]
     };
 
