492b7555dd14f06f3d4c67634356dc0cca829427
hieu <kaoha45@gmail.com>
2025-08-25 14:31:10 +0700

    video1920

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -10,22 +10,31 @@ class MyComponent extends React.Component {
         age: 25
     };
 
-    handleClick = (event) => {
-        console.log("clickmede");
-        console.log("toi la");
+   handleClick(event) {
+    console.log("click me de");
+    this.setState({
+        name: 'hieucao',
+        age: Math.floor(Math.random() * 100 + 1)
+        // address: 'hanoi',
+        // age: 25
+    });
 
-    }
+    //   this.setState({
+    //   age: Math.floor(Math.random() * 100 + 1)
+    // });
+}
+
 
     handleOnMouseOver = (event) => {
-        console.log(event.pageX);
+        // console.log(event.pageX);
     }
 
     render() {
         return (
             <div>
-                Tôi là {this.state.name} và tôi ở {this.state.address}
+                Tôi là {this.state.name} và tôi năm nay {this.state.age}
                 <button onMouseEnter={this.handleOnMouseOver}>Hover me</button>
-                <button onClick={this.handleClick}>Click me</button>
+                <button onClick={()=>(this.handleClick())}>Click me</button>
             </div>
         );
     }
